6d6b8e35915787d7a2a32694ac6f5817c73f4a50
Farhan  Rahman <farhanrahman02@gmail.com>
2020-05-09 02:10:55 +0530

    main file for data input for EEMD on EEG

--- data_eemg.m
+++ data_eemg.m
@@ -1,12 +1,14 @@
 dataset=load('2014.10.15_1.mat')
 y= dataset.EEG;
+N=672;
+fs=128;
 aim =8;                      % numbers of IMF
 NR =10;                      % value of ensemble
 Nstd = 0.3;                  % param to white noise
-% imf=eemd_e(y,aim,NR,Nstd);
+IMF =eemd_e(y,aim,NR,Nstd);
 plot(y)
 xlim([0 672])
-% 
+%
 % % subplot(8,1,1)
 % % plot(y)
 % % xlim([0 672])
@@ -16,37 +18,28 @@ xlim([0 672])
 % % N=672;
 % % T=N/fs;
 % % figure();
-% % Y=abs(fft(eemd(y,aim,NR,Nstd),N)); 
+% % Y=abs(fft(eemd(y,aim,NR,Nstd),N));
 % % freq=(0:N-1)/T;
 % % plot(freq,Y);xlabel('Frequency');
 % % % xlim([0 30])
-% for j=1:NR
-%     for i=1:aim
-%         hold on 
+%for j=1:NR
+    for i=1:aim
+%         hold on
 %         subplot(8,1,i);
 %         plot(IMF(:,i), 'r');
 % %         title('IMF plot')
 %         xlim([0 672])
-% %     hold on
-% %     plot(1:0.8:2000, 0, 'k');
-% %     set(gca,'YLim',[-1,1],'YTick',[-1:1])
-%     end 
-% end
-% % Y =fft(detrend(IMF(:,2)));       %??FFT??
-% % N = length(Y);    %FFT???????
-% % power = abs(Y(1:N/2)).^2;  %????
-% % nyquist = 1/2;
-% % freq = (1:N/2)/(N/2)*nyquist; %???
-% % subplot(7,1,7);
-% % plot(freq,power); grid on     %??????
-% % xlabel('??')
-% % ylabel('??')
-% % title('????')
-% % % period = 1./freq;                %????
-% % % subplot(5,2,4);
-% % % plot(period,power); grid on  %??????????
-% % % ylabel('??')
-% % % xlabel('??')
-% % % title('??—????')
-% % % [mp,index] = max(power);       %???????????
-% % % T_mean=period(index); 
+        T=N/fs;
+        figure();
+        Y =fft(detrend(IMF(:,i)),N);
+%         Y=abs(fft(emd(y),N));
+        freq=(0:N-1)/T;
+        plot(freq,abs(Y));xlabel('Frequency')
+        xlim([0 34])
+
+%         figure
+%         plot(abs(Y), 'r')
+%     hold on
+%     plot(1:0.8:2000, 0, 'k');
+%     set(gca,'YLim',[-1,1],'YTick',[-1:1])
+    end
